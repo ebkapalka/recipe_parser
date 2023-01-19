38a76c173f7fc4917c58a2fe146610cc6f8d09ff
Eric Kapalka <e.bkapalka@gmail.com>
2023-01-18 17:23:43 -0700

    Addedtokenizing function to split HTML into GPT3-sized chunks

--- load_file.py
+++ load_file.py
@@ -1,2 +1,16 @@
+import json
+
+
 def load_secrets(path: str) -> dict:
-    ...
+    """
+    Loads a JSON file containing the API key
+    :param path: relative or absolute file path to JSON file
+    :return: dict of JSON data or empty dict
+    """
+    try:
+        with open(path) as f:
+            data = f.read()
+            return json.loads(data)
+    except Exception as e:
+        print(e)
+        return {}
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from process_html.sanitize import sanitize_html
+from process_html.tokenize import tokenize_html
+from load_file import load_secrets
 import openai
 
 if __name__ == '__main__':
     recipe_url = "https://www.joshuaweissman.com/post/" \
                  "the-easiest-noodle-dish-ever-yaki-udon"
-    print(sanitize_html(recipe_url).prettify())
+    html = sanitize_html(recipe_url).prettify()
+    tokens = tokenize_html(html)
+    secrets = load_secrets("secrets.json")
+    openai.api_key = secrets.get("OpenAI API Key", "Not Found")
+    print(tokens[0].__repr__())
